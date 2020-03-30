bdd6ebd2778bdf6b5eec2ce0a8534150fa80f23b
time <ilya@zenio.co>
2020-03-30 10:49:40 +0300

    refactoring

--- frontend/client/src/helpers/insert-by-position/insert-by-position.spec.ts
+++ frontend/client/src/helpers/insert-by-position/insert-by-position.spec.ts
@@ -4,13 +4,28 @@ import {EPosition} from '../../constants/shared';
 describe('testing fn insertByPosition', () => {
     const testID = 99;
 
-    it('testing insert into array by anchor and position', () => {
+    it('testing inserting into an array by anchor and position (BEFORE)', () => {
         const result = insertByPosition([1, 2, 3], testID, 3, EPosition.BEFORE);
         expect(result).toEqual([1, 2, testID, 3]);
     });
 
-    it('testing insert into array at the end if there is no anchor and position', () => {
+    it('testing inserting into an array by anchor and position (AFTER)', () => {
+        const result = insertByPosition([1, 2, 3], testID, 2, EPosition.AFTER);
+        expect(result).toEqual([1, 2, testID, 3]);
+    });
+
+    it('testing inserting into an array at the end if there is no anchor and position', () => {
         const result = insertByPosition([1, 2, 3], testID);
+        expect(result).toEqual([1, 2, 3, testID]);
+    });
+
+    it('testing inserting into an array by anchor but without position', () => {
+        const result = insertByPosition([1, 2, 3], testID, 3);
+        expect(result).toEqual([1, 2, 3, testID]);
+    });
+
+    it('testing inserting into an array without an anchor but with a position', () => {
+        const result = insertByPosition([1, 2, 3], testID, undefined, EPosition.AFTER);
         expect(result).toEqual([1, 2, 3, testID]);
     });
 });
