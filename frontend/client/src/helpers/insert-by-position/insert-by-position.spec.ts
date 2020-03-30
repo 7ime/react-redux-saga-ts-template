667b0cc486bfea24c06c144343745f36fc8524fb
time <ilya@zenio.co>
2020-03-30 10:43:03 +0300

    refactoring

--- frontend/client/src/helpers/insert-by-position/insert-by-position.spec.ts
+++ frontend/client/src/helpers/insert-by-position/insert-by-position.spec.ts
@@ -4,12 +4,12 @@ import {EPosition} from '../../constants/shared';
 describe('testing fn insertByPosition', () => {
     const testID = 99;
 
-    it('Insert into array by anchor and position ', () => {
+    it('testing insert into array by anchor and position', () => {
         const result = insertByPosition([1, 2, 3], testID, 3, EPosition.BEFORE);
         expect(result).toEqual([1, 2, testID, 3]);
     });
 
-    it('Insert into array at the end if there is no anchor and position', () => {
+    it('testing insert into array at the end if there is no anchor and position', () => {
         const result = insertByPosition([1, 2, 3], testID);
         expect(result).toEqual([1, 2, 3, testID]);
     });
